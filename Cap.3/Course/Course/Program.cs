d5cb0711c287a85209e92e9d463244dd00a5f0c2
Caio André <caio_andre15@hotmail.com>
2021-04-05 20:05:16 -0300

    Capitulo 2. adicionado

---
+++ Cap.3/Course/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n1 = 254;
+            int n2 = 1000;
+            int n3 = 25555;
+            long n4 = 12121515L;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
