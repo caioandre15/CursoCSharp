2fe9e472b74f37cd465e2bf82c11a736f6e54b6c
Caio André <caio_andre15@hotmail.com>
2021-04-19 22:05:36 -0300

    Cap.3 Saida de dados - Teoria

--- Cap.3/Course/Course/Program.cs
+++ Cap.3/Course/Course/Program.cs
@@ -6,10 +6,31 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            bool completo = false;
+            char genero = 'F';
+            float n5 = 4.5f;
+            double n6 = 5.4;
             byte n1 = 254;
             int n2 = 1000;
             int n3 = 25555;
             long n4 = 12121515L;
+            string nome = "Maria Green";
+            object obj1 = "Eduardo";
+            object obj2 = 2.23;
+            int n7 = int.MinValue;
+            int n8 = int.MaxValue;
+            decimal n9 = decimal.MaxValue;
+
+            Console.WriteLine(n9);
+            Console.WriteLine(n8);
+            Console.WriteLine(n7);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(nome);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(n6);
+            Console.WriteLine(n5);
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
---
+++ Cap.3/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saida de dados (Console)
+            
+            // Imprimindo texto
+            Console.Write("Bom dia!");//Sem Quebra de Linha
+            Console.WriteLine("Boa Tarde!");//Com quebra de linha
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("--------------------------");
+            
+            //Imprimindo variáveis 
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+
+            //Place Holders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais.",nome,idade,saldo);
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais.");
+            //Concatenação de Strings
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
