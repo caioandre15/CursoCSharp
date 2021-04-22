55b09186540cff0c746fddd0cfecf5096c6bfe91
Caio André <caio_andre15@hotmail.com>
2021-04-22 20:46:51 -0300

    Cap.3 Conversao Implicita e Casting

---
+++ Cap.3/Conversao_Implicita_E_Casting/Conversao_Implicita_E_Casting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conversao_Implicita_E_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 4.5f;
+
+            double y = x;
+
+            Console.WriteLine(y);
+
+            int a = 5;
+            int b = 2;
+
+            double resultado = (double)a / b;
+
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
